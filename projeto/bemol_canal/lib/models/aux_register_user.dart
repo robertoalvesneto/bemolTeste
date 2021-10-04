708ad84c77f162ee415159f20cbfcd894b8e849a
robertoalvesneto <robertoalvesnetoprodut@gmail.com>
2021-10-04 18:58:30 -0400

    classe auxiliar que facilita o trafego dos dados entre as telas

---
+++ projeto/bemol_canal/lib/models/aux_register_user.dart
@@ -0,0 +1,20 @@
+class RegisterUser {
+  /// Armazenando temporariamente os valores para passar entre os widgets e
+  /// continuar seu preenchimento.
+
+  String? name;
+  String? birth;
+  String? gender;
+  String? cpf;
+  String? rg;
+  String? number;
+
+  RegisterUser({
+    this.name,
+    this.birth,
+    this.gender,
+    this.cpf,
+    this.rg,
+    this.number,
+  });
+}
