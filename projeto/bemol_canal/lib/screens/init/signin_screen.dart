2ad392209c537393825cae0336f771fb6ceb537a
robertoalvesneto <robertoalvesnetoprodut@gmail.com>
2021-10-05 09:21:47 -0400

    login

--- projeto/bemol_canal/lib/screens/init/signin_screen.dart
+++ projeto/bemol_canal/lib/screens/init/signin_screen.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:bemol_canal/screens/init/widgets/forms/custom_textformfield.dart';
+
+import 'package:bemol_canal/constants/screen_size.dart';
+import 'package:bemol_canal/constants/colors.dart';
+import 'package:bemol_canal/constants/font_size.dart';
+
 class SignInScreen extends StatefulWidget {
   final ValueChanged<int> onComeBackButtonPressed;
 
@@ -7,30 +15,198 @@ class SignInScreen extends StatefulWidget {
       : super(key: key);
 
   @override
-  _SignInScreenState createState() =>
-      _SignInScreenState();
+  _SignInScreenState createState() => _SignInScreenState();
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        color: Colors.amber,
-        child: Center(
+      body: SingleChildScrollView(
+        child: Container(
+          width: Screen.sizeWidth(context: context),
+          height: Screen.sizeHeight(context: context),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("Logar"),
-              ElevatedButton(
-                onPressed: () => widget.onComeBackButtonPressed(1),
-                child: const Text("Voltar"),
-              )
+              Container(
+                child: Column(
+                  children: [
+                    _logo(),
+                    _message(),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: ConstColors.liteBlue,
+                  borderRadius: BorderRadius.only(
+                    topLeft: const Radius.circular(40),
+                    topRight: Radius.circular(150),
+                  ),
+                ),
+                width: Screen.sizeWidth(context: context),
+                height: Screen.sizeHeight(context: context, dividedBy: 2.5),
+                child: _login(),
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  // --- FUNCTIONS
+  void _handlerLoginButton() {
+    if (_formKey.currentState!.validate()) {
+      final email = _emailController.text;
+      final password = _passwordController.text;
+
+      // Logar usuario
+      _loginUser(email, password);
+    }
+  }
+
+  void _loginUser(String email, String password) {
+    final _authFire = FirebaseAuth.instance;
+
+    _authFire
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((user) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Sucesso ao logar!'),
+      ));
+
+      Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false);
+    }).catchError((onError) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Houve um erro, por favor, tente novamente.'),
+      ));
+    });
+  }
+
+  // --- WIDGETS
+  Widget _logo() {
+    return Container(
+      margin: EdgeInsets.only(top: 30),
+      width: Screen.sizeWidth(
+          context: context,
+          multipliedBy: (Screen.ratio(context) < 9 / 16) ? 0.6 : 0.5),
+      child: Image.asset("assets/images/logo_bemol.png"),
+    );
+  }
+
+  Widget _message() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        text: "Bem vindo de volta!\nInsira seu ",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: FontSize.headline,
+        ),
+        children: <TextSpan>[
+          TextSpan(
+            text: "email",
+            style: TextStyle(
+              color: ConstColors.red,
+            ),
+          ),
+          TextSpan(
+            text: " e",
+            style: TextStyle(
+              color: ConstColors.black,
+            ),
+          ),
+          TextSpan(
+            text: " senha",
+            style: TextStyle(
+              color: ConstColors.blue,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _login() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _email(),
+            _password(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _loginButton(),
+                _goBackButton(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _email() {
+    return CustomTextFormField(
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'O campo não pode ser vazio';
+        } else if (!value.contains('@')) {
+          return 'Email invalido';
+        }
+
+        return null;
+      },
+      isLogin: true,
+      textInputType: TextInputType.emailAddress,
+      controller: _emailController,
+      labelText: "Email",
+      hintText: "joao123@gmail.com",
+      icon: Icons.email,
+    );
+  }
+
+  Widget _password() {
+    return CustomTextFormField(
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'O campo não pode ser vazio';
+        } else if (value.length <= 7) {
+          return 'Muito fraca';
+        }
+        return null;
+      },
+      isLogin: true,
+      textInputType: TextInputType.visiblePassword,
+      controller: _passwordController,
+      labelText: "Senha",
+      hintText: "******",
+      icon: Icons.vpn_key,
+    );
+  }
+
+  Widget _goBackButton() {
+    return ElevatedButton(
+      onPressed: () => widget.onComeBackButtonPressed(1),
+      child: const Text("Voltar"),
+    );
+  }
+
+  Widget _loginButton() {
+    return ElevatedButton(
+      onPressed: () => _handlerLoginButton(),
+      child: const Text('Continuar'),
     );
   }
 }
--- projeto/bemol_canal/lib/screens/init/widgets/forms/custom_textformfield.dart
+++ projeto/bemol_canal/lib/screens/init/widgets/forms/custom_textformfield.dart
@@ -14,7 +14,12 @@ Widget CustomTextFormField({
   double? width,
   IconData? icon,
   String? prefixText,
+  bool? isLogin,
 }) {
+  /// Acredito que o 'isLogin' nao seja a solucao mas elegante, contudo
+  /// ele economiza bastante espaco, pois essas cores teriam que ser passadas
+  /// pela instancia e teria que ter o tratamento essa classe.
+
   return Container(
     width: width ?? null,
     padding: EdgeInsets.symmetric(vertical: 20),
@@ -25,6 +30,7 @@ Widget CustomTextFormField({
       controller: controller,
       inputFormatters: inputFormatters,
       decoration: textFieldStyle(
+        isLogin: (isLogin == true) ? true : false,
         icon: icon,
         prefixText: prefixText,
         labelText: labelText,
@@ -34,14 +40,26 @@ Widget CustomTextFormField({
   );
 }
 
-InputDecoration textFieldStyle(
-    {IconData? icon,
-    String? prefixText,
-    String labelText = "",
-    String hintText = ""}) {
+InputDecoration textFieldStyle({
+  IconData? icon,
+  String? prefixText,
+  String labelText = "",
+  String hintText = "",
+  bool isLogin = false,
+}) {
   return InputDecoration(
+    filled: isLogin,
+    fillColor: Colors.white,
     prefixText: prefixText,
-    prefixIcon: (icon == null) ? null : Icon(icon),
+    labelStyle: TextStyle(
+      color: isLogin ? Colors.black45 : null,
+    ),
+    prefixIcon: (icon == null)
+        ? null
+        : Icon(
+            icon,
+            color: isLogin ? Colors.black45 : null,
+          ),
     contentPadding: const EdgeInsets.only(left: 10),
     labelText: labelText,
     hintText: hintText,
@@ -52,8 +70,9 @@ InputDecoration textFieldStyle(
     enabledBorder: const OutlineInputBorder(
       borderSide: const BorderSide(color: Colors.black45, width: 1.4),
     ),
-    focusedBorder: const OutlineInputBorder(
-      borderSide: BorderSide(color: ConstColors.blue, width: 1.4),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+          color: isLogin ? ConstColors.blue : ConstColors.black, width: 1.4),
     ),
     errorBorder: const OutlineInputBorder(
       borderSide: BorderSide(color: ConstColors.red, width: 1.4),
